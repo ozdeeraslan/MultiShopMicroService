4a85f2cfac1bf91d5a8aeee133a9631c64f57612
ozdeeraslan <ozdenureraslan@gmail.com>
2024-08-20 22:19:04 +0200

    Add project files.

---
+++ Services/Catalog/MultiShop.Catalog/Dtos/CategoryDtos/CreateCategoryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiShop.Catalog.Dtos.CategoryDtos
+{
+    public class CreateCategoryDto
+    {
+        // id is automatic generated
+
+        public string CategoryName { get; set; }
+    }
+}

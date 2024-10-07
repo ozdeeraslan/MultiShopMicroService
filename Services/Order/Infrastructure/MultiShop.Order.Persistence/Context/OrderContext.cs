a845607f25dbc5299f9dec0ee920cf0599d226de
ozdeeraslan <ozdenureraslan@gmail.com>
2024-10-07 21:50:03 +0200

    IRepository-Repository-CQRS

---
+++ Services/Order/Infrastructure/MultiShop.Order.Persistence/Context/OrderContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Persistence.Context
+{
+    public class OrderContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;initial Catalog=MultiShopOrderDb;integrated Security=True");
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        public DbSet<Ordering> Orderings { get; set; }
+
+        
+    }
+}

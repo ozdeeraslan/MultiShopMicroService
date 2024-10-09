398c27c7192ed7915b997f1c730a362e858fd13d
ozdeeraslan <ozdenureraslan@gmail.com>
2024-10-09 22:39:00 +0200

    OrderDetails CQRS

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/OrderDetailHandlers/CreateOrderDetailCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Commands.OrderDetailCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers
+{
+    public class CreateOrderDetailCommandHandler
+    {
+        private readonly IRepository<OrderDetail> _repository;
+
+        public CreateOrderDetailCommandHandler(IRepository<OrderDetail> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(CreateOrderDetailCommand cCommand)
+        {
+            await _repository.CreateAsync(new OrderDetail
+            {
+                OrderingId = cCommand.OrderingId,
+                ProductAmount = cCommand.ProductAmount,
+                ProductId = cCommand.ProductId,
+                ProductName = cCommand.ProductName,
+                ProductPrice = cCommand.ProductPrice,
+                ProductTotalPrice = cCommand.ProductTotalPrice
+            });
+
+
+
+
+        }
+    }
+}

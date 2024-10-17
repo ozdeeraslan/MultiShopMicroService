04af369cf9707e46f18c34d7e4e0751e09bb9bf4
ozdeeraslan <ozdenureraslan@gmail.com>
2024-10-17 20:20:58 +0200

    OrderDb Migration with Portainer-Docker

--- Services/Order/Infrastructure/MultiShop.Order.Persistence/Context/OrderContext.cs
+++ Services/Order/Infrastructure/MultiShop.Order.Persistence/Context/OrderContext.cs
@@ -13,7 +13,7 @@ namespace MultiShop.Order.Persistence.Context
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=.;initial Catalog=MultiShopOrderDb;integrated Security=True");
+            optionsBuilder.UseSqlServer("Server=localhost,1440;initial Catalog=MultiShopOrderDb;User=sa;Password=Ozde16051994!");
         }
 
         public DbSet<Address> Addresses { get; set; }

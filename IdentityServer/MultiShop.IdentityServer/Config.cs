75f1547203769be7e266a851ea87545f83a2895a
ozdeeraslan <ozdenureraslan@gmail.com>
2024-10-24 19:53:37 +0200

    token config

--- IdentityServer/MultiShop.IdentityServer/Config.cs
+++ IdentityServer/MultiShop.IdentityServer/Config.cs
@@ -25,7 +25,10 @@ namespace MultiShop.IdentityServer
             new ApiResource("ResourceOrder")
             {
                 Scopes = {"OrderFullPermission"},
-            }
+            },
+
+            new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
+
         };
 
         public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[]
@@ -41,6 +44,8 @@ namespace MultiShop.IdentityServer
             new ApiScope("CatalogReadPermission", "Reading authority for catalog operations"),
             new ApiScope("DiscountFullPermission", "Full authority for discount operations"),
             new ApiScope("OrderFullPermission", "Full authority for order operations"),
+            new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
+
         };
 
         public static IEnumerable<Client> Clients => new Client[]
@@ -52,7 +57,7 @@ namespace MultiShop.IdentityServer
                 ClientName = "Multi Shop Visitor User",
                 AllowedGrantTypes = GrantTypes.ClientCredentials,
                 ClientSecrets = { new Secret("multishopsecret".Sha256()) },
-                AllowedScopes = { "CatalogReadPermission" }
+                AllowedScopes = { "DiscountFullPermission" }
             },
 
             //Manager
